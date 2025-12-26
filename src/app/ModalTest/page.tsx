10598450697143893cf03f7f4604750c42eeec33
홍요한 <ghddygks45@gmail.com>
2025-12-26 17:08:44 +0900

    feat: 모달 컴포넌트 브랜치 작성 및 최초 커밋 테스트

---
+++ src/app/ModalTest/page.tsx
@@ -0,0 +1,11 @@
+import '@/styles/reset.css';
+
+import Modal from '@/components/Modals/Modal';
+
+export default async function ModalTest() {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+}
---
+++ src/components/Modals/Modal.tsx
@@ -0,0 +1,6 @@
+'use client';
+import '@/components/design.css';
+
+export default function Modal() {
+  return <div className="text-3xl-bold">테스트</div>;
+}
