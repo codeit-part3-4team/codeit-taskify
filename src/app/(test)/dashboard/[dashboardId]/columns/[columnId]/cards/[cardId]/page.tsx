7d3a86adee982f8d3d3a0e8b175d548d2866f77f
홍요한 <ghddygks45@gmail.com>
2026-01-02 17:29:02 +0900

    fix: 버셀 빌드에서 문제가 된 폴더 삭제

--- src/app/(test)/dashboard/[dashboardId]/columns/[columnId]/cards/[cardId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function CardDetailPage({ params }: { params: { cardId: string } }) {
-  const { cardId } = await params;
-
-  console.log(cardId);
-
-  return <div>card detail {params.cardId}</div>;
-}
