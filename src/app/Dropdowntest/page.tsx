933bd59dc07ae9fe376799de8182ab257efab854
choigaeul <choigaeul@suwon.ac.kr>
2026-01-02 19:47:12 +0900

    feat:dropdown progress 1차 구현 완료

--- src/app/Dropdowntest/page.tsx
+++ src/app/Dropdowntest/page.tsx
@@ -1,26 +1,39 @@
 "use client";
 
-import DropdownMenu, { DropdownItem } from "@/components/Dropdown/dropdown-menu/dropdownmenu";
+// import DropdownMenu, { DropdownItem } from "@/components/Dropdown/dropdown-menu/dropdownmenu";
 
 
-export default function DropdownMenuTest() {
-  const items: DropdownItem[] = [
-    { id: "edit", label: "수정하기" },
-    { id: "delete", label: "삭제하기", variant: "danger" },
-  ];
+// export default function DropdownMenuTest() {
+//   const items: DropdownItem[] = [
+//     { id: "edit", label: "수정하기" },
+//     { id: "delete", label: "삭제하기", variant: "danger" },
+//   ];
+
+//   return (
+//     <div style={{ padding: 40 }}>
+//       <DropdownMenu
+//         items={items}
+//         highlightedId="edit"
+//         triggerLabel="•••"
+//         align="left"
+//         onSelect={(item) => {
+//           console.log("selected:", item);
+//           alert(`${item.label} 클릭`);
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
+import DropdownProgress, { ProgressValue } from "@/components/Dropdown/dropdown-progress/dropdownprogress";
+
+export default function Page() {
+  const [value, setValue] = useState<ProgressValue>("todo");
 
   return (
     <div style={{ padding: 40 }}>
-      <DropdownMenu
-        items={items}
-        highlightedId="edit"
-        triggerLabel="•••"
-        align="left"
-        onSelect={(item) => {
-          console.log("selected:", item);
-          alert(`${item.label} 클릭`);
-        }}
-      />
+      <DropdownProgress value={value} onChange={setValue} />
     </div>
   );
 }
