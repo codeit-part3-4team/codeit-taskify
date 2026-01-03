eda89576255b378af505d98b12ecedad62077fea
홍요한 <ghddygks45@gmail.com>
2026-01-04 00:50:36 +0900

    fix: 동적 라우트 매개변수 처리([dashboardId]/edit/page.tsx)

--- src/app/(test)/root/dashboard/[dashboardId]/edit/page.tsx
+++ src/app/(test)/root/dashboard/[dashboardId]/edit/page.tsx
@@ -1,4 +1,5 @@
-export default async function edit({ params }: { params: Promise<{ dashboardId: string }> }) {
+export default async function EditPage({ params }: { params: Promise<{ dashboardId: string }> }) {
   const { dashboardId } = await params;
+
   return <div>대시보드 수정페이지</div>;
 }
