d4621917d4d55fdd6fc2bfbb93ae9141fbfe54d6
홍요한 <ghddygks45@gmail.com>
2026-01-06 03:50:07 +0900

    api: 컬럼, 카드 데이터 받아옴

--- src/app/dashboard/[dashboardId]/dashboard.api.ts
+++ src/app/dashboard/[dashboardId]/dashboard.api.ts
@@ -1,20 +1,8 @@
+import { GetCardsResponse } from "./card.type";
+import { ColumnListResponse } from "./column.type";
 
 const API_URL = "https://sp-taskify-api.vercel.app/20-4";
 const TASKIFY_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NjQ4NywidGVhbUlkIjoiMjAtNCIsImlhdCI6MTc2Njc0NDY0NSwiaXNzIjoic3AtdGFza2lmeSJ9.HOjAslVpV5SaFhwCb1ankip7UyjIQ3hEDPgQHrKCnFg"
-
-export type ColumnListResponse = {
-  result: 'SUCCESS' | 'FAIL';
-  data: ColumnServer[];
-};
-
-export type ColumnServer = {
-  id: number;
-  title: string;
-  teamId: string;
-  dashboardId: number;
-  createdAt: string;
-  updatedAt: string;
-};
 
 
 export async function getColumns(
@@ -22,6 +10,30 @@ export async function getColumns(
 ): Promise<ColumnListResponse> {
   const res = await fetch(
     `${API_URL}/columns?dashboardId=${dashboardId}`,
+    {
+      method: 'GET',
+      cache: 'no-store',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${TASKIFY_ACCESS_TOKEN}`,
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch dashboard detail');
+  }
+
+  return res.json();
+}
+
+
+
+export async function getCards(
+  columnId: number
+): Promise<GetCardsResponse> {
+  const res = await fetch(
+    `${API_URL}/cards?columnId=${columnId}`,
     {
       method: 'GET',
       cache: 'no-store',
--- src/app/dashboard/[dashboardId]/page.tsx
+++ src/app/dashboard/[dashboardId]/page.tsx
@@ -1,3 +1,5 @@
+import { getCards, getColumns } from './dashboard.api';
+
 export default async function DashboardDetailPage({
   params,
 }: {
@@ -5,5 +7,26 @@ export default async function DashboardDetailPage({
 }) {
   const { dashboardId } = await params;
 
-  return <div>{dashboardId}</div>;
+  const responseColumData = await getColumns(dashboardId);
+  const columns = responseColumData.data;
+
+  const responseCardData = await Promise.all(columns.map((column) => getCards(column.id)));
+
+  const columnCards = responseCardData.map((columns) => columns.cards);
+
+  return (
+    <div>
+      {dashboardId}
+      {columns.map((column) => (
+        <p key={column.id}>{column.title}</p>
+      ))}
+      {columnCards.map((cardsInColumn, columnIndex) => (
+        <div key={columnIndex}>
+          {cardsInColumn.map((card) => (
+            <p key={card.id}>{card.title}</p>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
